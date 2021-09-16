dd3891f35919fb7711957be738059bd9600fb993
mdnurakmal <mdnurakmal@gmail.com>
2021-09-16 23:41:33 +0800

    update

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// api url
+const api_url = 
+      "https://cloud-resume-challenge-ouvnbq3x2q-de.a.run.app/increment";
+  
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
+  
+// Function to hide the loader
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = 
+        `<tr>
+          <th>Name</th>
+         </tr>`;
+    
+    // Loop to access all rows 
+    for (let r of data.list) {
+        tab += `<tr> 
+    <td>${r.count} </td>     
+</tr>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById("employees").innerHTML = tab;
+}
