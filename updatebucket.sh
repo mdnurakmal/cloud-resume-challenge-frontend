94d55877773d9490256e490afdcd04628143e300
mdnurakmal <mdnurakmal@gmail.com>
2021-09-21 17:26:05 +0800

    Update updatebucket.sh

--- updatebucket.sh
+++ updatebucket.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #check if bucket exists and not empty
-gsutil -q stat gs://${$PROJECT_NUMBER}-cloud-resume-challenge-frontend-bucket/index.html
+gsutil -q stat gs://$PROJECT_NUMBER-cloud-resume-challenge-frontend-bucket/index.html
 
 return_value=$?
 
 if [[ $return_value == 0 ]] 
 then
     echo "deleting existing FE folder"
-    gsutil rm -r gs://${$PROJECT_NUMBER}-cloud-resume-challenge-frontend-bucket
+    gsutil rm -r gs://$PROJECT_NUMBER-cloud-resume-challenge-frontend-bucket
 fi
 
-gsutil mb gs://${$PROJECT_NUMBER}-cloud-resume-challenge-frontend-bucket/
+gsutil mb gs://$PROJECT_NUMBER-cloud-resume-challenge-frontend-bucket/
 echo "uploading FE folder"
 
-gsutil cp -r ./* gs://${$PROJECT_NUMBER}-cloud-resume-challenge-frontend-bucket/
+gsutil cp -r ./* gs://$PROJECT_NUMBER-cloud-resume-challenge-frontend-bucket/
 
 #allow public acess to the bucket
-gsutil iam ch allUsers:objectViewer gs://${$PROJECT_NUMBER}-cloud-resume-challenge-frontend-bucket
+gsutil iam ch allUsers:objectViewer gs://$PROJECT_NUMBER-cloud-resume-challenge-frontend-bucket
 
 #set default static page for the bucket
-gsutil web set -m index.html -e 404.html gs://${$PROJECT_NUMBER}-cloud-resume-challenge-frontend-bucket
+gsutil web set -m index.html -e 404.html gs://$PROJECT_NUMBER-cloud-resume-challenge-frontend-bucket
